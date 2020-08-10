9a3438b547083f65476d6e99ec9bc746de5fd5c2
Samuel-dot-cloud <successam336@gmail.com>
2020-08-10 18:24:42 +0300

    Creating a classic POJO

--- app/src/main/java/com/studiofive/myedu/ScreenItem.java
+++ app/src/main/java/com/studiofive/myedu/ScreenItem.java
@@ -1,5 +1,36 @@
 package com.studiofive.myedu;
 
 public class ScreenItem {
+String Title, Description;
+int Screening;
 
+    public ScreenItem(String title, String description, int screening) {
+        Title = title;
+        Description = description;
+        Screening = screening;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getScreening() {
+        return Screening;
+    }
+
+    public void setScreening(int screening) {
+        Screening = screening;
+    }
 }
