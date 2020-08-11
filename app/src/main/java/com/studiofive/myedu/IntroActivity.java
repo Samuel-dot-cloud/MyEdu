71359fb6b9493563cc4a58d71a78c0c22da800a0
Samuel-dot-cloud <successam336@gmail.com>
2020-08-11 15:38:51 +0300

    Setting up tablayout with viewpager

--- app/src/main/java/com/studiofive/myedu/IntroActivity.java
+++ app/src/main/java/com/studiofive/myedu/IntroActivity.java
@@ -5,17 +5,23 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class IntroActivity extends AppCompatActivity {
     private ViewPager screenPager;
     IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        //initiate views
+        tabIndicator = findViewById(R.id.tab_indicator);
 
         //fill list screen
         List<ScreenItem> mList = new ArrayList<>();
@@ -27,6 +33,9 @@ public class IntroActivity extends AppCompatActivity {
         screenPager = findViewById(R.id.screen_viewpager);
         introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
         screenPager.setAdapter(introViewPagerAdapter);
+
+        //setup tablayout with viewpager
+        tabIndicator.setupWithViewPager(screenPager);
 
 
     }
