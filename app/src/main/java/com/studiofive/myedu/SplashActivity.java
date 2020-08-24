cd1ce0b3f93fa72948a0c8cac786a5d5213c7dfc
Samuel-dot-cloud <successam336@gmail.com>
2020-08-24 18:52:56 +0300

    Binding spash screen views and animations

--- app/src/main/java/com/studiofive/myedu/SplashActivity.java
+++ app/src/main/java/com/studiofive/myedu/SplashActivity.java
@@ -3,12 +3,36 @@ package com.studiofive.myedu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIMER = 5000;
+
+    @BindView(R.id.background_image)
+    ImageView mBackgroundImage;
+    @BindView(R.id.powered_by_line)
+    TextView mPoweredBy;
+
+    Animation sideAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        mBackgroundImage.setAnimation(sideAnim);
+        mPoweredBy.setAnimation(bottomAnim);
     }
 }
