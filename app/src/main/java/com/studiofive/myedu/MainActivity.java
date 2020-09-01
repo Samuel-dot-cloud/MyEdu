c65f7f1f9ee0dd710e7e42620ce8665529cf71d5
Samuel-dot-cloud <successam336@gmail.com>
2020-09-01 20:12:24 +0300

    Solving binding view error

--- app/src/main/java/com/studiofive/myedu/MainActivity.java
+++ app/src/main/java/com/studiofive/myedu/MainActivity.java
@@ -18,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.drawer_layout)
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         //Toolbar
         setSupportActionBar(toolbar);
