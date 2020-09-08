615fa393b26cb07150ca9a3c4a8d60977385c0a8
Samuel-dot-cloud <successam336@gmail.com>
2020-09-08 17:44:29 +0300

    Adding function to change fragment

--- app/src/main/java/com/studiofive/myedu/utils/Functions.java
+++ app/src/main/java/com/studiofive/myedu/utils/Functions.java
@@ -1,4 +1,15 @@
 package com.studiofive.myedu.utils;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.studiofive.myedu.R;
+
 public class Functions {
+    public static void changeMainFragment(FragmentActivity fragmentActivity, Fragment fragment){
+        fragmentActivity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container, fragment)
+                .commit();
+    }
 }
