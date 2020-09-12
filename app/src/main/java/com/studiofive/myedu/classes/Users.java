243037e492aed9a3db76a7be7e12e5c1f054cc28
Samuel-dot-cloud <successam336@gmail.com>
2020-09-12 19:08:03 +0300

    Creating a simple users class for app

--- app/src/main/java/com/studiofive/myedu/classes/Users.java
+++ app/src/main/java/com/studiofive/myedu/classes/Users.java
@@ -1,4 +1,38 @@
 package com.studiofive.myedu.classes;
 
 public class Users {
+    public  String name, personalMantra, image;
+
+    public Users() {
+    }
+
+    public Users(String name, String personalMantra, String image) {
+        this.name = name;
+        this.personalMantra = personalMantra;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonalMantra() {
+        return personalMantra;
+    }
+
+    public void setPersonalMantra(String personalMantra) {
+        this.personalMantra = personalMantra;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
