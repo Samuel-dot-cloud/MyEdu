446dcd00ca8b80273b00ab0d7b495579ea3a1657
Samuel-dot-cloud <successam336@gmail.com>
2020-09-12 19:40:32 +0300

    Working on instantiating fields in profile fragment

--- app/src/main/java/com/studiofive/myedu/fragments/ProfileFragment.java
+++ app/src/main/java/com/studiofive/myedu/fragments/ProfileFragment.java
@@ -2,14 +2,24 @@ package com.studiofive.myedu.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.studiofive.myedu.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +29,13 @@ import com.studiofive.myedu.R;
 public class ProfileFragment extends Fragment {
     private boolean isOpen = false;
     private ConstraintSet layout1, layout2;
+    private ConstraintLayout constraintLayout;
+    private Unbinder unbinder;
+
+    @BindView(R.id.circular_photo)
+    CircleImageView circlePhoto;
+//    @BindView(R.id.constraint_layout)
+//    ConstraintLayout constraintLayout;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -43,6 +60,28 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        //Changing status bar color to transparent
+        Window w = getActivity().getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        layout1 = new ConstraintSet();
+        layout2 = new ConstraintSet();
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
