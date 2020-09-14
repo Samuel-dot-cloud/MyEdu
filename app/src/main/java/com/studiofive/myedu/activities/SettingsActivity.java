c1968ee12941e13c3b416107e1187b8eb3a28b1a
Samuel-dot-cloud <successam336@gmail.com>
2020-09-14 20:37:27 +0300

    Attempting to solve error with toolbar

--- app/src/main/java/com/studiofive/myedu/activities/SettingsActivity.java
+++ app/src/main/java/com/studiofive/myedu/activities/SettingsActivity.java
@@ -1,16 +1,29 @@
 package com.studiofive.myedu.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.studiofive.myedu.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SettingsActivity extends AppCompatActivity {
+    @BindView(R.id.settings_toolbar)
+    Toolbar mSettingsToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mSettingsToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setTitle("Settings");
+
     }
 }
