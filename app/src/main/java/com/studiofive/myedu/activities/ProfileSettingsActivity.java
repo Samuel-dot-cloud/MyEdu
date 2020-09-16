204a85a81f6252247bd00691df18dcaba1df4212
Samuel-dot-cloud <successam336@gmail.com>
2020-09-16 12:49:23 +0300

    Adding a profile settings activity for profile update

---
+++ app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.studiofive.myedu.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.studiofive.myedu.R;
+
+public class ProfileSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
