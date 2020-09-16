c2096bf60a1e9ba9e53e14244ef859a2181d78e8
Samuel-dot-cloud <successam336@gmail.com>
2020-09-16 13:09:34 +0300

    Adding an app glide module for handling of images

---
+++ app/src/main/java/com/studiofive/myedu/adapters/MyEduAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.studiofive.myedu.adapters;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyEduAppGlideModule extends AppGlideModule {
+}
