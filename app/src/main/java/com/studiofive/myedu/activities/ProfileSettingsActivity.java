9637c1e12705834ee8c55564234b322cc1a5b1ce
Samuel-dot-cloud <successam336@gmail.com>
2020-09-16 15:59:58 +0300

    Adding a bottom sheet for editing names

--- app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
@@ -3,14 +3,22 @@ package com.studiofive.myedu.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.studiofive.myedu.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileSettingsActivity extends AppCompatActivity {
@@ -24,6 +32,12 @@ public class ProfileSettingsActivity extends AppCompatActivity {
     EditText mPersonalMantra;
     @BindView(R.id.update_settings_button)
     Button mUpdateProfileSettings;
+
+    private FirebaseUser mFirebaseUser;
+    private FirebaseFirestore mFirestore;
+    private Uri imageUri;
+    private ProgressDialog mProgressDialog;
+    private StorageReference mImageRef;
     private static final int GALLERY_PIC = 5;
 
     @Override
@@ -36,5 +50,10 @@ public class ProfileSettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Profile Settings");
+
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mFirestore = FirebaseFirestore.getInstance();
+        mImageRef = FirebaseStorage.getInstance().getReference().child("Profile Images");
+        mProgressDialog = new ProgressDialog(this);
     }
 }
