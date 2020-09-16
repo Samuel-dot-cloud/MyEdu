b5345e68cc656c4b7d6d7cb371fce31a79051895
Samuel-dot-cloud <successam336@gmail.com>
2020-09-16 12:58:33 +0300

    Trying to set up toolbar for profile settings

--- app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
@@ -1,16 +1,26 @@
 package com.studiofive.myedu.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.studiofive.myedu.R;
 
+import butterknife.BindView;
+
 public class ProfileSettingsActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Profile Settings");
     }
 }
