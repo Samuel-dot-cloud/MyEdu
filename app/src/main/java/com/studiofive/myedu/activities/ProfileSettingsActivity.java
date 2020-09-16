5b76fb9b71eb682d7e727ccb0d8ea2990ddeb255
Samuel-dot-cloud <successam336@gmail.com>
2020-09-16 13:16:02 +0300

    Binding views in profile settings activity

--- app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/studiofive/myedu/activities/ProfileSettingsActivity.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.studiofive.myedu.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileSettingsActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.set_profile_image)
+    CircleImageView mProfileImage;
+    @BindView(R.id.set_user_name)
+    EditText mUserName;
+    @BindView(R.id.set_personal_mantra)
+    EditText mPersonalMantra;
+    @BindView(R.id.update_settings_button)
+    Button mUpdateProfileSettings;
+    private static final int GALLERY_PIC = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
