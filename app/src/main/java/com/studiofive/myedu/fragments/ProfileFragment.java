067109f8b4df156be48f764e1e8410993b3bcccc
Samuel-dot-cloud <successam336@gmail.com>
2020-09-17 13:33:51 +0300

    Displaying name and image successfully

--- app/src/main/java/com/studiofive/myedu/fragments/ProfileFragment.java
+++ app/src/main/java/com/studiofive/myedu/fragments/ProfileFragment.java
@@ -76,7 +76,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getProfileInfo();
     }
 
 
@@ -107,6 +106,7 @@ public class ProfileFragment extends Fragment {
         mFirestore = FirebaseFirestore.getInstance();
 
         initActionClick();
+        getProfileInfo();
 
         return view;
     }
