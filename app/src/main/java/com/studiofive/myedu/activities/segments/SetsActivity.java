dbdb71b1b15b7c7edbe520ed6d6d0229e61b5f1e
Samuel-dot-cloud <successam336@gmail.com>
2020-10-24 19:43:04 +0300

    Setting up the Sets activity

--- app/src/main/java/com/studiofive/myedu/activities/segments/SetsActivity.java
+++ app/src/main/java/com/studiofive/myedu/activities/segments/SetsActivity.java
@@ -1,14 +1,35 @@
 package com.studiofive.myedu.activities.segments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.studiofive.myedu.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SetsActivity extends AppCompatActivity {
+    @BindView(R.id.category_expanded_image)
+    ImageView expandedImage;
+    @BindView(R.id.category_expanded_title)
+    TextView expandedTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sets);
+        ButterKnife.bind(this);
+
+        // Recieve data
+        Intent intent = getIntent();
+        String Title = intent.getExtras().getString("Title");
+        int image = intent.getExtras().getInt("Image") ;
+
+        expandedTitle.setText(Title);
+        expandedImage.setImageResource(image);
     }
 }
