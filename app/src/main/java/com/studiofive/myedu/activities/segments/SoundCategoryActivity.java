ed4e1047fd47d6d1d9a4dfa64e6add09bb89b67c
Samuel-dot-cloud <successam336@gmail.com>
2020-11-25 20:47:24 +0300

    Working on the sound game by creating a class and layout for objects.

--- app/src/main/java/com/studiofive/myedu/activities/segments/SoundCategoryActivity.java
+++ app/src/main/java/com/studiofive/myedu/activities/segments/SoundCategoryActivity.java
@@ -1,6 +1,8 @@
 package com.studiofive.myedu.activities.segments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +20,12 @@ import com.studiofive.myedu.fragments.categories.PlacesFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import es.dmoral.toasty.Toasty;
 
 public class SoundCategoryActivity extends AppCompatActivity {
     @BindView(R.id.sound_category_toolbar)
     Toolbar toolbar;
-    
+
     private String action;
 
     @Override
@@ -41,61 +44,65 @@ public class SoundCategoryActivity extends AppCompatActivity {
             if (savedInstanceState != null) {
                 return;
             }
-            
+
             navigationContext();
         }
     }
 
     private void navigationContext() {
-        if (action.compareTo("drinks") == 0){
+        if (action.compareTo("drinks") == 0) {
             DrinksFragment drinksFragment = new DrinksFragment();
             drinksFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, drinksFragment).commit();
 
             getSupportActionBar().setTitle("Drinks");
-        }else if (action.compareTo("foods") == 0){
+        } else if (action.compareTo("foods") == 0) {
             FoodsFragment foodsFragment = new FoodsFragment();
             foodsFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, foodsFragment).commit();
 
             getSupportActionBar().setTitle("Foods");
-        }else if (action.compareTo("fruits") == 0){
+        } else if (action.compareTo("fruits") == 0) {
             FruitsFragment fruitsFragment = new FruitsFragment();
             fruitsFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, fruitsFragment).commit();
 
             getSupportActionBar().setTitle("Fruits");
-        }else if (action.compareTo("animals") == 0){
+        } else if (action.compareTo("animals") == 0) {
             AnimalsFragment animalsFragment = new AnimalsFragment();
             animalsFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, animalsFragment).commit();
 
             getSupportActionBar().setTitle("Animals");
-        }else if (action.compareTo("places") == 0){
+        } else if (action.compareTo("places") == 0) {
             PlacesFragment placesFragment = new PlacesFragment();
             placesFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, placesFragment).commit();
 
             getSupportActionBar().setTitle("Places");
-        }else if (action.compareTo("electronics") == 0){
+        } else if (action.compareTo("electronics") == 0) {
             ElectronicsFragment electronicsFragment = new ElectronicsFragment();
             electronicsFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, electronicsFragment).commit();
 
             getSupportActionBar().setTitle("Electronics");
-        }else if (action.compareTo("clothing") == 0){
+        } else if (action.compareTo("clothing") == 0){
             ClothingFragment clothingFragment = new ClothingFragment();
             clothingFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.sound_fragment_container, clothingFragment).commit();
 
             getSupportActionBar().setTitle("Clothing");
+        }else {
+            Toasty.error(SoundCategoryActivity.this, "Something went wrong", Toast.LENGTH_SHORT, true).show();
+            Intent intent = new Intent(SoundCategoryActivity.this, SoundGameActivity.class);
+            startActivity(intent);
         }
     }
 }
